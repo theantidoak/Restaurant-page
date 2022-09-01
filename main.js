5e200b842a26810d5c310c77316f4b9624cd6f11
theantidoak <tcdoak@gmail.com>
2022-09-01 21:54:31 +0200

    Set up webpack and files

---
+++ main.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('Hey, I\'m using webpack');
+
+console.log('npm, also');
+
+console.log('and nodeJS');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBOztBQUVBOztBQUVBLDBCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdGF1cmFudC1wYWdlLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdIZXksIElcXCdtIHVzaW5nIHdlYnBhY2snKTtcblxuY29uc29sZS5sb2coJ25wbSwgYWxzbycpO1xuXG5jb25zb2xlLmxvZygnYW5kIG5vZGVKUycpOyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
